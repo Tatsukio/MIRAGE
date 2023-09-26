caeda75836fb15adf4f7a52220cd8b376a609093
Kristian Simov <krisi.simov@gmail.com>
2023-09-26 21:45:45 +0300

    added a batch tool which parses all paraworld cfg files in a directory, checking them for errors

---
+++ GameFiles/Basic/Tools/ParseAll.bat
@@ -0,0 +1,38 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+set /a parseCount=0
+set /a cfgFilesToParse=0
+set /a ttFilesToParse=0
+set errorlevel=
+echo Calculating pw cfg files in directory...
+for /r %%i in (*.txt, *.cfg) do (
+	set /a cfgFilesToParse+=1
+)
+for /r %%i in (*.ttree) do (
+	set /a ttFilesToParse+=1
+)
+echo ...done.
+
+set /a totalFilesToParse=cfgFilesToParse+ttFilesToParse
+set /a filesLeft=totalFilesToParse
+
+echo Wait for the cfgeditor to parse through all pw cfg files
+echo Cfg files to parse: !cfgFilesToParse!
+echo TTree files to parse: !ttFilesToParse!
+
+echo Parsing pw cfg files (.txt --- .cfg --- .ttree)...
+for /r %%i in (*.txt, *.cfg, *.ttree) do (
+	
+	set /a parseCount+=1
+	set /a filesLeft=filesLeft-1
+	
+	set /A percent=parseCount*100/totalFilesToParse
+	title Files left: !filesLeft!, progress: !percent!%%
+	
+	echo Parse file #!parseCount!: "%%i"
+	echo Parse file #!parseCount!: "%%i" >> parse_log.txt 2>&1
+	echo Y|start /w /b "" "CfgEditor.exe" "%%i" >> parse_log.txt 2>&1
+	
+)
+echo ...done, see parse_log.txt
+pause
